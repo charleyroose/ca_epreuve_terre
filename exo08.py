7f4ddeee65643b27b766d6ddd2bf05b8d7b425fb
charleyroose <charley.roose@gmail.com>
2023-03-27 18:22:29 +0200

    exo08terre

---
+++ exo08.py
@@ -0,0 +1,20 @@
+import sys
+
+try:
+	nb_entier = int(sys.argv[1])
+	exposant = int(sys.argv[2])
+# Vérification du nombre d'arguments
+	if len(sys.argv) != 3:
+		print("Erreur, il ne peut y avoir que deux arguments")
+		sys.exit()
+
+except (ValueError, IndexError):
+	print("Erreur, les arguments donné doivent être des nombres entier")
+	sys.exit()
+else:
+	if exposant <= 0:
+		print("Erreur, l'exposant ne peut pas être supérieur ou égal à 0")
+	else:
+		result = nb_entier ** exposant
+		print("Le résultat est", result)
+

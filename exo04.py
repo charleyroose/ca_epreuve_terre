e72d664d3679482a5a04232b537135d68c9dc255
charleyroose <charley.roose@gmail.com>
2023-04-11 19:42:31 +0200

    exo04terremodif

--- exo04.py
+++ exo04.py
@@ -1,17 +1,17 @@
 import sys
 
 # j'ajoute l'instruction "try-except" dans le cas où l'argument donné n'est pas un bn entier
+def pair_impair():
+    try:
+        var = int(sys.argv[1]) # je convertis l'argument donné en nb entier (int)
 
-try:
-    var = int(sys.argv[1]) # je convertis l'argument donné en nb entier (int)
-
-    if var % 2 == 0:
-        print("pair")
-
-    else:
-        print("impair")
+        if var % 2 == 0:
+            print("pair")
+        else:
+            print("impair")
 
 # si argument donné n'est pas un nb entier, alors cette string sera print
 
-except (IndexError, ValueError):
-    print("Tu ne me la mettras pas à l’envers.")
+    except (IndexError, ValueError):
+        print("Tu ne me la mettras pas à l’envers.")
+pair_impair()

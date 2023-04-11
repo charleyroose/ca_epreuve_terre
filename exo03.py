4910c0044ee350f96b1e514c6391698e79ca4de2
charleyroose <charley.roose@gmail.com>
2023-04-11 18:53:08 +0200

    exo03terremodif

--- exo03.py
+++ exo03.py
@@ -1,12 +1,15 @@
 import sys
 
-letter = sys.argv[1]
-arg = range(ord('a'), ord('z')+1)
-var = letter
+def alphabet_from(letter):
+    arg = range(ord(letter), ord('z')+1)
+    var = letter
 
-for num in arg:
-	print(var, end="")			#sort chaque itération de la boucle en enlevant le retour à la ligne
-	if var == "z":				#stop la boucle lorsque la valeur = z
-		break
-	var = chr(ord(var) + 1)		#transforme la lettre en valeur numérique, ajoute 1 et transforme le résultat en lettre correspondante
-print()							#ajoute un retour à la ligne
+    for num in arg:
+        print(var, end="")
+        if var == "z":
+            break
+        var = chr(ord(var) + 1)
+    print()
+
+letter = sys.argv[1]
+alphabet_from(letter)

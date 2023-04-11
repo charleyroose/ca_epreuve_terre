ba9ee8ad3286ae97fd320c6e5ae6d6a8637afc08
charleyroose <charley.roose@gmail.com>
2023-04-11 17:07:08 +0200

    exo02terremodif

--- exo02.py
+++ exo02.py
@@ -1,3 +1,8 @@
 import sys
-for n in sys.argv[1:]: #j'utilise une boucle for pour parcourir la liste d'arguments séparement, je commence par l'index 1 sinon j'ai le nom du scipt
-	print(n)
+
+def separate_arg():
+	for n in sys.argv[1:]: #j'utilise une boucle for pour parcourir la liste d'arguments séparement, je commence par l'index 1 sinon j'ai le nom du scipt
+		print(n)
+	return
+
+separate_arg()

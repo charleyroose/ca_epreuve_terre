be3179382710417eb07abfaa6f2bf9825b76c6a4
charleyroose <charley.roose@gmail.com>
2023-04-12 18:10:20 +0200

    exo06terremodif

--- exo06.py
+++ exo06.py
@@ -1,8 +1,12 @@
 import sys
 
-try:
-	arg = sys.argv[1]
-except (IndexError, ValueError):
-	print("Erreur")
-else:
-	print(arg[::-1])	#j'utilise cette syntaxe pour extraire une sous-séquence de 'arg' en commençant par la fin, en allant jusqu'au début en itérant de manière croissante
+def reverse_string():
+	try:
+		arg = sys.argv[1]
+	except (IndexError, ValueError):
+		print("Erreur")
+		sys.exit()
+	else:
+		print(arg[::-1])	#j'utilise cette syntaxe pour extraire une sous-séquence de 'arg' en commençant par la fin, en allant jusqu'au début en itérant de manière croissante
+		sys.exit()
+reverse_string()

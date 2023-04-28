f4c340f924f4afac3b5ddb4eacdc7bb4dc6ef351
charleyroose <charley.roose@gmail.com>
2023-04-28 15:58:34 +0200

    exo09terremodif

--- exo09.py
+++ exo09.py
@@ -1,20 +1,22 @@
 import sys
 
-try:
-	nb_entier = int(sys.argv[1])
-	#vérification du nombre d'arguments
-	if len(sys.argv) != 2:
-		print("Erreur, il ne peut y avoir qu'un seul argument.")
-		sys.exit()
-	#Vérification d'un entier positif
-	if nb_entier <= 0:
-		print("Erreur, l'argument donné doit être positif.")
-		sys.exit()
+def square_root():	
+	try:
+		nb_entier = int(sys.argv[1])
+		#vérification du nombre d'arguments
+		if len(sys.argv) != 2:
+			print("Erreur, il ne peut y avoir qu'un seul argument.")
+			sys.exit()
+		#Vérification d'un entier positif
+		if nb_entier <= 0:
+			print("Erreur, l'argument donné doit être positif.")
+			sys.exit()
 
-except (IndexError, ValueError):
-	print("Erreur, l'argument donné doit être un nombre entier.")
-	sys.exit()
-#calcul d'une racine carré est égale à nb_entier exposant 1/2
-else:
-	result = nb_entier ** 0.5
-	print(result)
+	except (IndexError, ValueError):
+		print("Erreur, l'argument donné doit être un nombre entier.")
+		sys.exit()
+	#calcul d'une racine carré est égale à nb_entier exposant 1/2
+	else:
+		result = nb_entier ** 0.5
+		print(result)
+square_root()

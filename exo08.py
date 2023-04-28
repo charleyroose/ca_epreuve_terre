ae995ad1cf116a03635a244ef5d3694f86633c21
charleyroose <charley.roose@gmail.com>
2023-04-28 15:45:11 +0200

    exo08terremodif

--- exo08.py
+++ exo08.py
@@ -1,20 +1,23 @@
 import sys
 
-try:
-	nb_entier = int(sys.argv[1])
-	exposant = int(sys.argv[2])
-# Vérification du nombre d'arguments
-	if len(sys.argv) != 3:
-		print("Erreur, il ne peut y avoir que deux arguments")
+def power_result():
+
+	try:
+		nb_entier = int(sys.argv[1])
+		exposant = int(sys.argv[2])
+	# Vérification du nombre d'arguments
+		if len(sys.argv) != 3:
+			print("Erreur, il ne peut y avoir que deux arguments")
+			sys.exit()
+
+	except (ValueError, IndexError):
+		print("Erreur, les arguments donné doivent être des nombres entier")
 		sys.exit()
-
-except (ValueError, IndexError):
-	print("Erreur, les arguments donné doivent être des nombres entier")
-	sys.exit()
-else:
-	if exposant <= 0:
-		print("Erreur, l'exposant ne peut pas être supérieur ou égal à 0")
 	else:
-		result = nb_entier ** exposant
-		print("Le résultat est", result)
-
+		if exposant <= 0:
+			print("Erreur, l'exposant ne peut pas être supérieur ou égal à 0")
+		else:
+			result = nb_entier ** exposant
+			print("Le résultat est", result)
+			
+power_result()

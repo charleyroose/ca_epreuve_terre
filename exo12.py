e36252579cfab46a999bd8b7ffa9d72016489308
charleyroose <charley.roose@gmail.com>
2023-05-05 17:52:13 +0200

    exo12terremodif

--- exo12.py
+++ exo12.py
@@ -1,40 +1,41 @@
 import sys
 
-try:
-	hh, mm, = map(int, sys.argv[1].split(":"))
-	period = sys.argv[2]
+def convert_time_24h(hh, mm, period):
+	if hh == 12 and period == "AM":
+		hh = 0
+		return "{0:02d}:{1:02d}".format(hh, mm)
 
+	elif hh == 12 and period == "PM":
+		return "{0:02d}:{1:02d}".format(hh, mm)
+
+	elif hh < 12 and period == "PM":
+		hh = hh + 12
+		return "{0:02d}:{1:02d}".format(hh, mm)
+
+	elif period == "AM":
+		return "{0:02d}:{1:02d}".format(hh, mm)
+
+try:
 	if len(sys.argv) != 3:
 		print("Erreur, il ne peut y avoir que deux arguments sous format hh:mm période")
 		sys.exit()
-	if hh >= 13:
-		print("Erreur, l'heure ne peut pas dépasser 12")
-		sys.exit()
-	if hh < 0:
-		print("Erreur, l'heure doit être un entier positif")
-		sys.exit()
-	if mm < 0:
-		print("Erreur, les minutes doivent être un entier positif")
-		sys.exit()
-	if mm >= 60:
-		print("Erreur, les minutes ne peuvent pas dépasser 59")
+
+	hh, mm, = map(int, sys.argv[1].split(":"))
+	period = sys.argv[2]
+
+	if hh < 0 or hh >= 13:
+		print("Erreur, l'heure doit être entre 0 et 12")
 		sys.exit()
 
+	if mm < 0 or mm >= 60:
+		print("Erreur, les minutes doivent être entre 0 et 59")
+		sys.exit()
+
+	if not period.isupper():
+		print("Erreur, la période doit être écrite en majuscule")
+		sys.exit()
 except (IndexError, ValueError):
 	print("Erreur, l'argument donné doit être sous format hh:mm période")
 	sys.exit()
 
-else:
-	if hh == 12 and period == "am":
-		hh = 0
-		print("{0:02d}:{1:02d}".format(hh, mm))
-
-	elif hh == 12 and period == "pm":
-		print("{0:02d}:{1:02d}".format(hh, mm))
-
-	elif hh < 12 and period == "pm":
-		hh = hh + 12
-		print("{0:02d}:{1:02d}".format(hh, mm))
-
-	elif period == "am":
-		print("{0:02d}:{1:02d}".format(hh, mm))
+print(convert_time_24h(hh, mm, period))

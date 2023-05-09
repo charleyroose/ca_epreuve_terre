ba0b4f365e1e2e110fe5aca6d892254f3bea9a70
charleyroose <charley.roose@gmail.com>
2023-05-09 16:18:22 +0200

    exo14terremodif

--- exo14.py
+++ exo14.py
@@ -1,25 +1,25 @@
 import sys
 
+def est_triee(liste):
+    # Vérifie si la liste est triée
+    for i in range(len(liste) - 1):
+        if liste[i] > liste[i + 1]:
+            return False
+    return True
+
 try:
     if len(sys.argv) < 3:
         print("Erreur, la liste doit contenir au moins 2 arguments.")
         sys.exit()
-    #stocker l'état du tri
-    est_triee = True
 
-    #convertir la première entrée en entier
-    int(sys.argv[1])
+    # Convertir les arguments en entiers
+    liste_entiers = [int(arg) for arg in sys.argv[1:]]
 
-    #parcourir la liste d'arguments
-    for n in range(2, len(sys.argv)):
-        if int(sys.argv[n]) < int(sys.argv[n-1]):
-            est_triee = False
-            break
-
-    if est_triee:
+    if est_triee(liste_entiers):
         print("Triée !")
     else:
         print("Pas triée !")
 
 except (ValueError, IndexError):
     print("Erreur, la liste ne doit contenir que des nombres entiers.")
+    sys.exit()

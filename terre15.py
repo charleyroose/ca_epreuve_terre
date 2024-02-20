9302a3000eb9e7f39218d520a3e8081bace21b1c
charleyroose <charley.roose@gmail.com>
2024-02-20 11:53:09 +0100

    Terre_Ok

---
+++ terre15.py
@@ -0,0 +1,20 @@
+'''
+Terre : ok
+
+Créez un programme qui célèbre votre victoire.
+
+
+Exemples d’utilisation :
+$> python exo.py
+J’ai terminé l’Épreuve de la Terre et c’était [].
+$>
+
+Note : [] est à remplacer par un adjectif de votre choix (facile ?)
+'''
+
+import sys
+
+def earth_feeling():
+    result = "J'ai terminé l'Épreuve de la Terre et c'était plutôt fun !"
+    return result
+print(earth_feeling())
